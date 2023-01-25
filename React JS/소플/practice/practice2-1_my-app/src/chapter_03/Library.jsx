a576277722b08e0318e3a33b93bc9c01d25f3e88
Son-Sumin <goldkitty007@gmail.com>
2023-01-25 18:03:47 +0900

    REACT

---
+++ React JS/소플/practice/practice2-1_my-app/src/chapter_03/Library.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Book from "./Book";
+
+function Library(props) {
+    return (
+        <div>
+            <Book name="처음 만난 파이썬" numOfPage={300} />
+            <Book name="처음 만난 AWS" numOfPage={400} />
+            <Book name="처음 만난 리액트" numOfPage={500} />
+        </div>
+    );
+}
+
+export default Library;
+
+// Book.jsx보다 상위 컴포넌트인 Library.jsx
+// Library.jsx는 3개의 Book 컴포넌트를 렌더링하고 있다
+// 다음으로 index.js 파일 수정하기
--- React JS/소플/practice/practice2-1_my-app/src/index.js
+++ React JS/소플/practice/practice2-1_my-app/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from './chapter_03/Library';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
 
@@ -15,3 +16,13 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+/*
+수정 및 추가
+import Library from './chapter_03/Library';
+<Library />
+
+수정한 내용은,
+Library 컴포넌트를 가져온 뒤에 리액트 DOM을 사용하여 Root DOM에 렌더링하도록 하는 코드
+*/
