97fde2904affb3cbf651b592249b07e17f8b3c6b
Son-Sumin <goldkitty007@gmail.com>
2023-01-26 16:15:01 +0900

    REACT

--- React JS/소플/practice/practice2-1_my-app/src/index.js
+++ React JS/소플/practice/practice2-1_my-app/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
+import CommentList from "./chapter_05/CommentList";
 
 // chapter_03
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,14 +17,22 @@ import Clock from './chapter_04/Clock';
 // );
 
 // chapter_04
-setInterval(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}, 1000);
+// setInterval(() => {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>,
+//     document.getElementById('root')
+//   );
+// }, 1000);
+
+// chapter_05
+ReactDOM.render(
+      <React.StrictMode>
+        <CommentList />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
