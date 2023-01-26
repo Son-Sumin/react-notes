c4682e187688ba57dbdb4710bd5b6ff3e6d3cea1
Son-Sumin <goldkitty007@gmail.com>
2023-01-26 12:47:01 +0900

    REACT

--- React JS/소플/practice/practice2-1_my-app/src/index.js
+++ React JS/소플/practice/practice2-1_my-app/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//import Library from './chapter_03/Library';
+import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
 
 // chapter_03
