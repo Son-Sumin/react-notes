67df4d09993b95e2065c31cd05058c3e6e45d51b
Son-Sumin <goldkitty007@gmail.com>
2023-02-02 09:32:59 +0900

    REACT

---
+++ React JS/코딩앙마/05 CSS 작성법(module css).md
@@ -0,0 +1,5 @@
+## #5 CSS 작성법(module css)
+
+- create-react-app으로 만든 프로젝트라면 별도 다른 패키지 설치 없이 바로 css 사용할 수 있는 방법 3가지   
+  * inline   
+    
--- React JS/코딩앙마/voca/src/component/Hello.js
+++ React JS/코딩앙마/voca/src/component/Hello.js
@@ -1,14 +1,13 @@
-import World from "./World";
+// import World from "./World";
 
-export default function Hello() {
-    return (
-        <div>
-            <h1>Hello</h1>
-            <World />
-            <World />
-        </div>
-    );
-}
+// export default function Hello() {
+//     return (
+//         <div>
+//             <h1 style={ color : 'red' }>Hello</h1>
+//             <World />
+//             <World />
+//         </div>
+//     );
+// }
 
-// <div> 안에 2개의 태그 넣음  →  jsx는 1개의 태그만 생성 가능
-// <div> 가 아닌 <> 빈 태그로 감싸도 됨
+// 태그 내 스타일 부여시 {키(속성) : } 객체로 작성
